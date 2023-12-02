e5313aa2078a928c911507355a502c8c6ac3a8e6
Nizamuddin4493 <nizamuddin4493@gmail.com>
2023-12-02 23:31:47 +0430

    give the validation to user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
