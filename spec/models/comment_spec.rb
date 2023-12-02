8c6bdb037ceef358c98bbc4f3601d1e094bfefbb
Nizamuddin4493 <nizamuddin4493@gmail.com>
2023-12-03 00:30:33 +0430

    removed linter errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -19,8 +19,8 @@ RSpec.describe Comment, type: :model do
 
   describe '#update_comments_counter' do
     it 'updates the post\'s comments_counter' do
-      create(:comment, post: post, author: user)
-      expect { create(:comment, post: post, author: user) }.to change { post.reload.comments_counter }.by(1)
+      create(:comment, post:, author: user)
+      expect { create(:comment, post:, author: user) }.to change { post.reload.comments_counter }.by(1)
     end
   end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -19,8 +19,8 @@ RSpec.describe Like, type: :model do
 
   describe '#update_likes_counter' do
     it 'updates the post\'s likes_counter' do
-      create(:like, post: post, user: user)
-      expect { create(:like, post: post, user: user) }.to change { post.reload.likes_counter }.by(1)
+      create(:like, post:, user:)
+      expect { create(:like, post:, user:) }.to change { post.reload.likes_counter }.by(1)
     end
   end
 end
